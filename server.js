eafed0dec3439b2567fb2b07caba6d8c9e200d3a
Kai R <kairichardson2000@gmail.com>
2019-07-01 14:42:09 -0700

    fixing heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 
-var PORT = process.env.PORT ||3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
@@ -17,8 +17,16 @@ app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
+// set up promises wirth mongoose
+mongoose.Promise = global.Promise;
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    "mongodb://user:password1@ds345587.mlab.com:45587/heroku_b2jj6kxn",
+  {
+    useNewUrlParser: true
+  }
+);
 
 // Routes
 require("./routes/html-routes")(app);
